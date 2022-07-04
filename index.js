f76c75708ec5ea620d7d21295441b193630a0364
Rami Inberg <rami.inberg@gmail.com>
2022-07-04 14:24:27 +0300

    handling if promise is rejected

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+const imageCreatorEl = document.getElementById('image-creator')
+
 // Muista piilottaa client id.. 
 fetch('https://api.unsplash.com/photos/random?orientation=landscape&query=nature&client_id=fmlNMcq0oHotgRDNfsvEt-Xcynv6wu1h34T7Y5k0_8A')
 .then(res => res.json())
@@ -6,6 +8,13 @@ fetch('https://api.unsplash.com/photos/random?orientation=landscape&query=nature
   console.log('data', data)
   const { urls, user } = data
   document.body.style.backgroundImage = `url(${urls.full})`
-  document.getElementById('image-creator').textContent = `By: ${user.name}`
+  imageCreatorEl.textContent = `By: ${user.name}`
+})
+.catch(() => {
+  document.body.style.backgroundImage = `url(
+    https://images.unsplash.com/photo-1493246507139-91e8fad9978e?crop=entropy&cs=tinysrgb&fm=jpg&ixid=MnwzNDMzNzJ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NTY5MzA5NzI&ixlib=rb-1.2.1&q=80
+    )`
+  imageCreatorEl.textContent = 'By: Vadim Sherbakov'
+
 })
 
