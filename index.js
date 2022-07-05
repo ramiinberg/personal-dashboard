e3579bb2a6fe0fb8bce28533e8ef237008bc2c1f
Rami Inberg <rami.inberg@gmail.com>
2022-07-05 11:31:11 +0300

    getting user current position.

--- index.js
+++ index.js
@@ -38,10 +38,23 @@ fetch('https://api.coingecko.com/api/v3/coins/dogecoin')
 
 setInterval(currentTime, 1000);
 
+const currentTimeEl = document.getElementById('time')
+
 function currentTime() {
   const current = new Date().toLocaleTimeString('fi-FI', { hour: '2-digit', minute: '2-digit' })
-  const currentTimeEl = document.getElementById('time')
   currentTimeEl.textContent = current
 }
+
+if('geolocation' in navigator) {
+  /* geolocation is available */
+  navigator.geolocation.getCurrentPosition((position) => {
+    console.log(position.coords.latitude, position.coords.longitude)
+    // doSomething(position.coords.latitude, position.coords.longitude);
+  });
+} else {
+  /* geolocation IS NOT available */
+}
+
+
 
 
